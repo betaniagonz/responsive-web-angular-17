4cec80b106e0ca80c0b88adf9a8afb59e84aa019
Betania Gonzalez <betaniagonz@gmail.com>
2024-03-03 00:04:52 -0500

    first version of the portfolio

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { MainContainerComponent } from '../layout/main-container';
+import { SocialMediaComponent } from "../social-media/social-media.component";
 
 @Component({
-  selector: 'app-welcome',
-  standalone: true,
-  imports: [MainContainerComponent],
-  templateUrl: './welcome.component.html',
-  styleUrl: './welcome.component.scss'
+    selector: 'app-welcome',
+    standalone: true,
+    templateUrl: './welcome.component.html',
+    styleUrl: './welcome.component.scss',
+    imports: [MainContainerComponent, SocialMediaComponent]
 })
 export class WelcomeComponent {
 
